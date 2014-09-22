e01b64da6886d190e64e59c6dd07d7a4d300c417
bogdan <bogdanbrkic@gmail.com>
2014-09-22 23:38:58 +0200

    grunt modernizr

--- gruntfile.js
+++ gruntfile.js
@@ -8,6 +8,72 @@ module.exports = function(grunt){
 
     pkg: grunt.file.readJSON('package.json'),
 
+
+//*************
+modernizr: {
+
+    dist: {
+        // [REQUIRED] Path to the build you're using for development.
+        "devFile" : "node_modules/grunt-modernizr/lib/modernizr-dev.js",
+
+        // [REQUIRED] Path to save out the built file.
+        "outputFile" : "build/js/modernizr-custom.js",
+
+        // Based on default settings on http://modernizr.com/download/
+        "extra" : {
+            "shiv" : true,
+            "printshiv" : false,
+            "load" : true,
+            "mq" : false,
+            "cssclasses" : true
+        },
+
+        // Based on default settings on http://modernizr.com/download/
+        /*
+          http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-shiv-mq-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-css_mediaqueries-css_resize-dataview_api-dom_dataset-file_api-getusermedia-notification-performance-script_async-script_defer-load
+        */
+
+        "extensibility" : {
+            "addtest" : true,
+            "prefixed" : true,
+            "teststyles" : true,
+            "testprops" : true,
+            "testallprops" : true,
+            "hasevents" : true,
+            "prefixes" : true,
+            "domprefixes" : true,
+            "fontface": true,
+            "fontface": true,
+        },
+
+        // By default, source is uglified before saving
+        "uglify" : true,
+
+        // Define any tests you want to implicitly include.
+        "tests" : [],
+
+        // By default, this task will crawl your project for references to Modernizr tests.
+        // Set to false to disable.
+        "parseFiles" : true,
+
+        // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
+        // You can override this by defining a "files" array below.
+        // "files" : {
+            // "src": []
+        // },
+
+        // When parseFiles = true, matchCommunityTests = true will attempt to
+        // match user-contributed tests.
+        "matchCommunityTests" : false,
+
+        // Have custom Modernizr tests? Add paths to their location here.
+        "customTests" : []
+    }
+
+},
+
+
+
 //*************
     cssc: {
       build: {
@@ -75,8 +141,9 @@ module.exports = function(grunt){
 
   });
 
-  grunt.registerTask('default',   []);
+  grunt.registerTask('default',   ['modernizr']);
   grunt.registerTask('buildcss',  ['sass', 'cssc', 'cssmin']);
   grunt.registerTask('buildjs',  ['concat', 'uglify']);
+
 
 };
