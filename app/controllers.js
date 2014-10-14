05963a2307f1ea7895454539b42f551e0c6cb1d8
bogdan <bogdanbrkic@gmail.com>
2014-10-14 23:03:05 +0200

    service

--- app/controllers.js
+++ app/controllers.js
@@ -7,31 +7,47 @@
 /* controler */
 appTumblr.controller('TumblrCtrl', function($scope, FetchDataFromServer) {
   //$scope.data = null;
-  console.log('controllers...Go............');
-  $scope.test = "test 123";
+  console.log('............controllers............');
+
   $scope.data = FetchDataFromServer.getDatafromAPI();
-  $scope.posts = $scope.data;
-  $scope.body = $scope.posts;
-  console.log($scope.data);
-  $scope.posts = data.response.posts;
+   //$scope.posts = $scope.data;
+  // $scope.body = $scope.posts;
+   console.log($scope);
+   //$scope.posts = data.response.posts;
 });
+
 
 
 
 /* service */
 appTumblr.factory('FetchDataFromServer', function($http, $q) {
 
-  console.log('.. Services js Go...................');
+  //console.log('.. Services js Go...................');
+return {
+    getDatafromAPI:function(scope) {
+      console.log('................ Services getDatafromAPI...................');
 
-    this.getDatafromAPI = function() {
-    console.log('.. Services getDatafromAPI Go...................');
-    url = '/tumblr-fetch-blog-data.php';
-
-    $http.get(url).success(function(data, status, headers, config) {
-        console.log(response.data);
-        return response.data;
-      });
-    };
-
+     $http.get('/tumblr-fetch-blog-data.php').success(function(data, status, headers, config) {
+         console.log(data);
+         //return response.data;
+         $scope.data = data;
+       });
+    }
+};
 
 });
+
+
+
+
+//***********
+ // app.factory('registerService', function ($http) {
+ //        return {
+ //         getYears:function (scope) {// scopes comes from your controller
+ //        $http({method : "GET",url : "interface.php"})
+ //        .success(function(data){
+ //                scope.data = data;!!!!!!
+ //             })
+ //             }
+ //          }
+ //       });
