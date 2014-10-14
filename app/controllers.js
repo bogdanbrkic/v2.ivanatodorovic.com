774bf88b089a9dea095a12cebe98279df926c2fe
bogdan <bogdanbrkic@gmail.com>
2014-10-14 21:21:34 +0200

    factory angular return value

--- app/controllers.js
+++ app/controllers.js
@@ -1,34 +1,37 @@
 /*
-* FetchTumblrPostsCtrl
-*
-*/
+ * FetchTumblrPostsCtrl
+ *
+ */
 'use strict';
 
-appTumblr.controller('TumblrCtrl', function ($scope, FetchDataFromServer) {
-            //$scope.data = null;
-            console.log('controllers...Go............');
-              $scope.test = "test 123";
-              $scope.data = FetchDataFromServer.getDatafromAPI();
-              // $scope.posts = $scope.data;
-              // $scope.body = $scope.posts;
-              // console.log($scope.data);
-              // $scope.posts = data.response.posts;
+/* controler */
+appTumblr.controller('TumblrCtrl', function($scope, FetchDataFromServer) {
+  //$scope.data = null;
+  console.log('controllers...Go............');
+  $scope.test = "test 123";
+  $scope.data = FetchDataFromServer.getDatafromAPI();
+  $scope.posts = $scope.data;
+  $scope.body = $scope.posts;
+  console.log($scope.data);
+  $scope.posts = data.response.posts;
 });
 
 
+
+/* service */
 appTumblr.factory('FetchDataFromServer', function($http, $q) {
 
-                console.log('.. Services js Go...................');
-                 //this.getDatafromAPI = function() {
-                       console.log('.. Services getDatafromAPI Go...................');
-                      // url = 'tumblr-fetch-blog-data';
-                    //  $http.get(url)
-                    //  .success(function(data,status,headers,config) {
-                    //    console.log(response.data);
-                    //   return response.data;
-                    //  });
+  console.log('.. Services js Go...................');
 
-                // };
+    this.getDatafromAPI = function() {
+    console.log('.. Services getDatafromAPI Go...................');
+    url = '/tumblr-fetch-blog-data.php';
+
+    $http.get(url).success(function(data, status, headers, config) {
+        console.log(response.data);
+        return response.data;
+      });
+    };
 
 
 });
