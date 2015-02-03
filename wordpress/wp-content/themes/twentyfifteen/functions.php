162e8cb3ef337c15c68da8d1ea20a594d2296fc5
bogdan <bogdanbrkic@gmail.com>
2015-02-03 20:17:28 +0100

    used new WP_Query ..
    and pagination is now working - custom

--- wordpress/wp-content/themes/twentyfifteen/functions.php
+++ wordpress/wp-content/themes/twentyfifteen/functions.php
@@ -363,6 +363,48 @@ function post_class_oddeven() {
 		echo $str;
 }
 
+
+/* paginatio n Fn() */
+
+function kriesi_pagination($pages = '', $range = 2)
+{
+		$showitems = ($range * 2)+1;
+
+		global $paged;
+		if(empty($paged)) $paged = 1;
+
+		if($pages == '')
+		{
+				global $wp_query;
+				print_r($paged);
+
+				$pages = $wp_query->max_num_pages;
+				if(!$pages)
+				{
+						$pages = 1;
+				}
+		}
+
+		if(1 != $pages)
+		{
+				echo "<div class='pagination'>";
+				if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo;</a>";
+				if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
+
+				for ($i=1; $i <= $pages; $i++)
+				{
+						if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+						{
+								echo ($paged == $i)? "<span class='current'>".$i."</span>":"<a href='".get_pagenum_link($i)."' class='inactive' >".$i."</a>";
+						}
+				}
+
+				if ($paged < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a>";
+				if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
+				echo "</div>\n";
+		}
+}
+
 /* AFC */
 // if(function_exists("register_field_group"))
 // {
--- wordpress/wp-content/themes/twentyfifteen/tpl-news-page-template.php
+++ wordpress/wp-content/themes/twentyfifteen/tpl-news-page-template.php
@@ -9,28 +9,22 @@
 
 <!-- content  -->
 
-
-		<?php while ( have_posts() ) : the_post(); ?>
-			<? //php the_content(); | php post_class_oddeven(); ?>
-
 					<?php
 					/*
-					 * list posts from cat == news
+					 * list posts from cat == news (2)
 					*/
-					// $paged = (get_query_var('page')) ? get_query_var('page') : 1;
-					$args = array( 'posts_per_page' => 3, 'category_name' => 'news','orderby' => 'post_date', 'offset'=> 0, 'post_status' => 'publish' , 'page' => $page);
-					$myposts = get_posts( $args );
+					$additional_loop = new WP_Query("posts_per_page=4&cat=2&paged=$paged");
 
 					// debug
-					// print_r($args);
-					// print_r($myposts);
+					// print_r($additional_loop);
+					?>
 
-					foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+					<?php while ($additional_loop->have_posts()) : $additional_loop->the_post(); ?>
 
 					<div class="news news__item">
 							<div class="col-1-2 news__img"><img src="<?php the_field('fg_news_img'); ?>" /></div>
 							<div class="col-1-2 news__txt p-lr-1">
-								<h3 class="news__txt-title"><?php the_title(); ?></h3>
+								<h3 class="news__txt-title"><?php the_title() ?></h3>
 								<p class="news__txt-p"><?php the_content(); ?></p>
 							</div>
 							</div>
@@ -40,42 +34,12 @@
 
 
 
-					<?php endforeach;
-								wp_reset_postdata();
-					?>
-
+					<?php wp_reset_postdata(); // Restore original Post Data ?>
 
 		<?php endwhile; // end of the loop. ?>
 
-		<div id="pagination">
-			<?php wp_pagenavi(); ?>
-		</div>
+		<!-- render pagination  -->
+			<?php kriesi_pagination($additional_loop->max_num_pages); ?>
 
 <!-- footer  -->
 <?php get_footer(); ?>
-
-
-
-
-
-
-
-
-<!-- tpl left right ... fali big post  -->
-<!-- <div class="news news__item">
-	<div class="col-1-2 news__img"><img src="http://ivanatodorovic.com/img/news/fotka-radionice.jpg" ></div>
-	<div class="col-1-2 news__txt p-lr-1">
-		<h3 class="news__txt-title">I was on a panel about future about Visual Anthropology with legend David MacDougall on Gottingen International Ethnographic Film Festival!</h3>
-		<p class="news__txt-p">My film “When I was a boy, I was a girl “ was screened too and was emotional journey for me as Gottingen was first film festival in 2006 where I showed my first film</p>
-	</div>
-</div>
-
-<div class="news__item"><div class="news__spacer">&nbsp;</div></div>
-
-<div class="news news__item">
-	<div class="col-1-2 news__txt p-lr-1">
-		<h3 class="news__txt-title">I was on a panel about future about Visual Anthropology with legend David MacDougall on Gottingen International Ethnographic Film Festival!</h3>
-		<p class="news__txt-p">My film “When I was a boy, I was a girl “ was screened too and was emotional journey for me as Gottingen was first film festival in 2006 where I showed my first film</p>
-	</div>
-	<div class="col-1-2 news__img"><img src="http://ivanatodorovic.com/img/news/fotka-radionice.jpg" ></div>
-</div> -->
